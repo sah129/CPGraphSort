21c8e917ff00ddfdc9f5c7736e994fc7a6f0e09d
sah129 <55058535+sah129@users.noreply.github.com>
2020-06-29 17:00:21 -0400

    Update css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
 import Data from './Data'
+import { flexbox } from '@material-ui/system';
 
 
 function App() {
   return (
     <div className="App">
+    <Box display="flex">
         <h1>CellProfiler Graph and Sort</h1>
+        </Box>
             <Data />
         <div className = "App-instructions">
         Instructions: jdks;fjdsfsd
