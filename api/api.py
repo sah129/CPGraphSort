0b4b27fa50f2b714e2dd3b019ab595382c1478bb
sah129 <55058535+sah129@users.noreply.github.com>
2020-07-18 02:28:32 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ import time
 from flask import Flask, request, send_file
 from generate_graph import generate_results
 
-flask_app = Flask(__name__, static_folder='./build', static_url_path='/')
+flask_app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 if __name__ == "__main__":
     flask_app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
